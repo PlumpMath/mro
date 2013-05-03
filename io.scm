a41e9a6fc8f9adc5fe558c0611bf3a2e54d17907
Do Nhat Minh <mrordinaire@gmail.com>
2013-05-03 23:26:48 +0800

    refactored n-bytes-copied into do bindings in copy-port

--- io.scm
+++ io.scm
@@ -6,11 +6,10 @@
 (define* (copy-port producer consumer #:optional (block-size 4096))
   "Copies from producer port to consumer port.
    Returns total number of bytes read from producer port."
-  (let ((buffer (make-generalized-vector 'u8 block-size))
-        (n-bytes-copied 0))
+  (let ((buffer (make-generalized-vector 'u8 block-size)))
     (do
       ((n-read (get-bytevector-n! producer buffer 0 block-size)
-               (get-bytevector-n! producer buffer 0 block-size)))
+               (get-bytevector-n! producer buffer 0 block-size))
+       (n-bytes-copied 0 (+ n-bytes-copied n-read)))
       ((eof-object? n-read) n-bytes-copied)
-      (put-bytevector consumer buffer 0 n-read)
-      (set! n-bytes-copied (+ n-bytes-copied n-read)))))
+      (put-bytevector consumer buffer 0 n-read))))
