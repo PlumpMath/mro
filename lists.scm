5ac020cfc790cd46d0f20f10ad827aefc669c04b
Do Nhat Minh <mrordinaire@gmail.com>
2013-05-06 11:38:26 +0800

    refactor list-take to use let iterate instead of a helper function

--- lists.scm
+++ lists.scm
@@ -4,11 +4,10 @@
 
 (define (list-take l n)
   "Takes n elements from the list l from the beginning. Returns the n-element list."
-  (define (helper l n acc)
-    (if (or (= 0 n) (null? l))
-      (reverse acc)
-      (helper (cdr l) (- n 1) (cons (car l) acc))))
-  (helper l n '()))
+  (let iterate ((l l) (n n) (acc '()))
+   (if (or (= 0 n) (null? l))
+     (reverse acc)
+     (iterate (cdr l) (- n 1) (cons (car l) acc)))))
 
 (define (random-elt l)
   "Returns a random element from the list l."
